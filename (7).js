01ae15576c69e61b9fa6659f86bb68c9df310bac
Denisssska <86466933+Denisssska@users.noreply.github.com>
2022-03-12 15:36:55 +0300

    Add files via upload

---
+++ (7).js
@@ -0,0 +1,13 @@
+function fourSeven(n){
+  let obj = {
+    4: 7,
+    7: 4,
+  }
+  return obj[n] || 0;
+//     // Your Code Here
+//  let arr = [4,7]
+//  let pos = arr.filter(function(f){return f !== n }) 
+//     // console.log(pos) 
+ 
+//  return pos 
+ }
